49e6321be79cc6a939f13164ba73d1a313b92da1
Mariana Niño Solano <mnino499@unab.edu.co>
2026-04-10 11:48:41 -0500

    Maquetacion Login Y Register

--- app/src/main/java/me/mariananino/exploracolombiaapp/MainActivity.kt
+++ app/src/main/java/me/mariananino/exploracolombiaapp/MainActivity.kt
@@ -12,36 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import me.mariananino.exploracolombiaapp.ui.theme.ExploraColombiaAPPTheme
+import me.mariananino.exploracolombiaapp.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExploraColombiaAPPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExploraColombiaAPPTheme {
-        Greeting("Android")
-    }
-}
