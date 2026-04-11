d3d144030f0838868f4146c0e854e3846edcbfa0
Mariana Niño Solano <mnino499@unab.edu.co>
2026-04-10 21:14:52 -0500

    configuración de la navegación

--- app/src/main/java/me/mariananino/exploracolombiaapp/MainActivity.kt
+++ app/src/main/java/me/mariananino/exploracolombiaapp/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import me.mariananino.exploracolombiaapp.ui.theme.ExploraColombiaAPPTheme
 import me.mariananino.exploracolombiaapp.LoginScreen
 
@@ -18,9 +21,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+                val myNavController = rememberNavController()
+                NavHost(
+                    navController = myNavController,
+                    startDestination = "login",
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    composable(route = "login"){
+                        LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+                    }
+
+                    composable(route = "register") {
+                        RegisterScreen(onRegisterSuccess = {}, onNavigateToLogin = {})
+
+                    }
+                }
+            }
         }
     }
-}
+
 
